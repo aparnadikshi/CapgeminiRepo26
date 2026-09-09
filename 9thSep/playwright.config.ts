2630b098f177d6db5357c0fe505118e815a2c3f1
aparnadikshi <132121802+aparnadikshi@users.noreply.github.com>
2026-09-09 12:50:49 +0530

    Add files via upload

---
+++ 9thSep/playwright.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+
+  testDir: './tests',
+
+  reporter: [
+    ['list'],
+    ['html'],
+    [
+      'json',
+      {
+        outputFile: 'test-results/results.json'
+      }
+    ]
+  ],
+
+  use: {
+
+    baseURL: 'https://phptravels.net/',
+
+    screenshot: 'only-on-failure',
+
+    trace: 'retain-on-failure',
+
+    video: 'retain-on-failure'
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome']
+      }
+    }
+  ]
+});
